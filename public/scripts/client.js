1281e1ee55c93e84162364980a684c4d0aa7ee2f
Heather <hmcleod29@gmail.com>
2022-05-26 21:52:40 -0400

    Add timeago feature and fetch tweets with AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,6 @@
  */
 
 $(() => {
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
   const createTweetElement = function (data) {
     const $tweet = $(` 
     <div class="hardcodedTweet">     
@@ -44,7 +19,9 @@ $(() => {
         <p>${data.content.text}</p>
    </div>
     <footer class="footerClass">
-      <span class="daysAgo">${data.created_at}</span> 
+    <div class="timeAgo">
+    <p>${timeago.format(data.created_at)}</p>
+  </div> 
        <div class="icons">
        <div class="icon1 icon">
        <p><i class="fa-solid fa-flag"></i></p>
@@ -70,22 +47,26 @@ $(() => {
     });
     return;
   };
-  renderTweets(data);
 
-  // console.log($tweet);
-  // $("#tweetsContainer").append($tweet);
+  const loadNewTweets = function () {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+    }).then((tweets) => {
+      $(".tweetcontainer").empty();
+      renderTweets(tweets);
+    });
+  };
+  loadNewTweets();
 
   $("#submit-form").on("submit", function (event) {
     event.preventDefault();
     const data = $(this).serialize();
-    console.log("data", data);
-
+    $("form").trigger("reset");
     $.ajax({
       method: "POST",
       url: "/tweets",
       data: data,
-    }).then(() => {
-      console.log("Worked");
-    });
+    }).then(() => {});
   });
 });
