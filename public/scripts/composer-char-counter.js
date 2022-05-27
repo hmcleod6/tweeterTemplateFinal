ac7f3265beced81e673317469bad5bf4c3e41154
Heather <hmcleod29@gmail.com>
2022-05-26 21:23:43 -0400

    Add character count feature to app

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  const $inputForm = $("#tweet-text");
+  let charactersRemaining = 140;
+
+  $inputForm.on("keyup", function (event) {
+    const lengthOfInput = $(this).val().length;
+    valueOfInput = this.value;
+
+    $counter = $(".counter");
+
+    if (lengthOfInput < 140) {
+      $counter.css("color", "#545149");
+      $counter.text(charactersRemaining - lengthOfInput);
+    } else {
+      $counter.css("color", "#FF0000");
+      $counter.text(charactersRemaining - lengthOfInput);
+    }
+  });
+});
